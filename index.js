2d4ac9fb12331fbc566d4b6ad7b79caffe3bd8f7
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-09-07 19:19:30 +0600

    delete

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 // midleware
@@ -19,12 +20,46 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("connect");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const collection = client.db("redpositive_data").collection("persons");
+    const dataCollection = client.db("redpositive_data").collection("data");
+
+    app.get("/person", async (req, res) => {
+      const query = {};
+      const cursor = collection.find(query);
+      const persons = await cursor.toArray();
+      res.send(persons);
+    });
+
+    app.post("/person", async (req, res) => {
+      const person = req.body;
+
+      const result = await collection.insertOne(person);
+
+      res.send(result);
+    });
+
+    app.delete("/data/:id", async (req, res) => {
+      const id = req.params;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+
+      const result = await collection.deleteOne(query);
+      res.send(result);
+    });
+    // app.delete("/data/is", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await collection.deleteOne(query);
+    //   res.send(result);
+    // });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hlw ki khobor");
@@ -33,7 +68,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("listen to port", port);
 });
-
-// midleware
-// app.use(cors());
-// app.use(express.json());
