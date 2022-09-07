785ff3232e72f69c2ef9148bc7f29498a428c3f9
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-09-07 09:30:12 +0600

    addmongo

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+const port = process.env.PORT || 4000;
 const app = express();
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+// redpositive1
+// 0LezrBHxs9HFjDjs
+
+const uri =
+  "mongodb+srv://redpositive1:0LezrBHxs9HFjDjs@cluster0.zdt5s4o.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("connect");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("hlw ki khobor");
